2b509b10fad4440f6654b9ae6d985c033b9a683b
waldohidalgo <whidalgohp@gmail.com>
2024-05-20 13:37:45 -0400

    change env

--- api/config/db.js
+++ api/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import "dotenv/config";
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Database Connected");
   } catch (err) {
     console.error(err.message);
